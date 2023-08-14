a6325cd83af57a4c48c6f4565600141effff83e9
Sonu Kumar <60311092+sonu2614@users.noreply.github.com>
2023-08-14 11:39:13 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,19 @@
 
 import React from "react";
 import './../styles/App.css';
+import Tabs from "./Tabs";
+
+const tabs=[
+    {title:"Tab 1" , content:"Tab 1"},
+    {title:"Tab 2" , content:"Tab 2"},
+    {title:"Tab 3" , content:"Tab 3"}
+]
 
 const App = () => {
   return (
     <div>
         {/* Do not remove the main div */}
+         <Tabs tabs={tabs}/>
     </div>
   )
 }
