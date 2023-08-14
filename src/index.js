debf62d3579351ef19528020bf29e1c4fcaf94c6
Sonu Kumar <60311092+sonu2614@users.noreply.github.com>
2023-08-14 11:34:05 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./components/App";
+import ReactDOM  from "react-dom";
 
+import App from "./App";
+import Tabs from "./Tabs"
 
-
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App/>, document.getElementById("root"));
